6aef055ee1a8444bf765b2ba00b734683214ae86
jinhwan <song7022556@gmail.com>
2026-01-26 20:25:34 +0900

    feat: 구글 도메인 등록

--- app/layout.tsx
+++ app/layout.tsx
@@ -64,6 +64,7 @@ export default function RootLayout({
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+        <meta name="google-site-verification" content="OvtOage0P7fVxwz_mXb0IuckkKDXvi4ZEDGXImXh_dw" />
       </Head>
       <body className={`${pretendard.className} bg-(--background) text-(--foreground)`}>
         <Providers>
