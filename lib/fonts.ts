a43639e0efbecb2ba6f58674f3f760f635be5f8c
jinhwan <song7022556@gmail.com>
2026-03-22 23:25:52 +0900

    perf: lighthouse 개선 및 폰트 woff2 전환

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -3,22 +3,22 @@ import localFont from 'next/font/local';
 export const pretendard = localFont({
   src: [
     {
-      path: '../public/fonts/Pretendard-Regular.otf',
+      path: '../public/fonts/Pretendard-Regular.woff2',
       weight: '400',
       style: 'normal',
     },
     {
-      path: '../public/fonts/Pretendard-Medium.otf',
+      path: '../public/fonts/Pretendard-Medium.woff2',
       weight: '500',
       style: 'normal',
     },
     {
-      path: '../public/fonts/Pretendard-SemiBold.otf',
+      path: '../public/fonts/Pretendard-SemiBold.woff2',
       weight: '600',
       style: 'normal',
     },
     {
-      path: '../public/fonts/Pretendard-Bold.otf',
+      path: '../public/fonts/Pretendard-Bold.woff2',
       weight: '700',
       style: 'normal',
     },
